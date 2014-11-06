65ac076e37db3475eb2c40facb2c326f95b7c7d2
Геннадий <gens-gens@mail.ru>
2014-11-06 18:48:03 +0200

    Animal

---
+++ Android/Task_1/app/libs/src/main/java/com/example/gens/myapplication_animal/Doc.java
@@ -0,0 +1,10 @@
+package com.example.gens.myapplication_animal;
+
+/**
+ * Created by Gens on 05.11.2014.
+ */
+public class Doc extends Animal {
+    public Doc(int id, String name) {
+        super(id, name);
+    }
+}
