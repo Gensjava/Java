4cd27e5c37773b7c2311520fa75368da51135c64
Геннадий <gens-gens@mail.ru>
2014-12-30 21:28:27 +0200

    service

--- Android/Task_8_Service_Binder/Java/SmsMonitor.java
+++ Android/Task_8_Service_Binder/Java/SmsMonitor.java
@@ -38,7 +38,7 @@ public class SmsMonitor extends BroadcastReceiver {
             String address = addressText.toString();
 
             //создаем и отправлеям mIntent текст, номер
-            Intent mIntent = new Intent(context, SmsMainActivityBinder.class);
+            Intent mIntent = new Intent(context, SmsService.class);
             mIntent.putExtra("sms_body", body);
             mIntent.putExtra("sms_address", address);
             //стартуем сервис
